4f7127a12427b5aff86280ebdb6fda1a1a761241
jhorman orozco <42872738+jhorman10@users.noreply.github.com>
2020-03-19 23:47:00 -0500

    Update firebase.js

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -2,14 +2,14 @@ import app from 'firebase/app';
 import 'firebase/firestore';
 
 const config = {
-  apiKey: "AIzaSyB6GoMUnLzjQ9f8gdRP6K6ovQvF4RlEzfw",
-  authDomain: "home-1d95f.firebaseapp.com",
-  databaseURL: "https://home-1d95f.firebaseio.com",
-  projectId: "home-1d95f",
-  storageBucket: "home-1d95f.appspot.com",
-  messagingSenderId: "298487454569",
-  appId: "1:298487454569:web:479f8288ba1a8466b2bbf8",
-  measurementId: "G-3NM9YH94ZJ"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 class Firebase {
